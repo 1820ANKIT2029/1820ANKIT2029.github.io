c521fbd15a0839769c862af7c2d169fdfee68e12
1820ANKIT2029 <ankitkumar00002250908@gmail.com>
2025-05-26 12:17:01 +0530

    resume link update

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ const config = {
   githubUsername: "1820ankit2029", // GitHub username
   linkedinHandle: "ankit-kumar-20233057-cse", // LinkedIn handle
   codeforceHandle: "AGENTVEER", // Codeforce handle
-  resumeGdriveLink: "https://drive.google.com/file/d/1ZmW9Jc4CgCZAlmXrUyQrZVAEVt-qKW8U",
+  resumeGdriveLink: "https://drive.google.com/file/d/1fgL5nS_ZPtwrYtoxYuKTBzE2_1tvpiC2",
   siteTitle: "My Portfolio",
 };
 
